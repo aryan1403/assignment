631ac58e04d33af87746cc51cea61cd3e26e129e
Aaryan <aaryan14032006@gmail.com>
2021-11-25 15:39:07 +0530

    Getter & Setters Added

--- demo/src/main/java/com/example/demo/response.java
+++ demo/src/main/java/com/example/demo/response.java
@@ -3,6 +3,24 @@ package com.example.demo;
 public class response {
     String responsecode;
     String msg;
+    public String getResponsecode() {
+        return responsecode;
+    }
+    public void setResponsecode(String responsecode) {
+        this.responsecode = responsecode;
+    }
+    public String getMsg() {
+        return msg;
+    }
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    public Object getOb() {
+        return ob;
+    }
+    public void setOb(Object ob) {
+        this.ob = ob;
+    }
     Object ob;
     public response(String responsecode, String msg, Object ob) {
         this.responsecode = responsecode;
